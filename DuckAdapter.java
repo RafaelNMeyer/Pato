12733bb6a96dfdde17a09b293d61af77a6588083
tobizen <48719850+tobizen@users.noreply.github.com>
2019-06-04 11:16:23 -0300

    Add files via upload

---
+++ DuckAdapter.java
@@ -0,0 +1,24 @@
+
+public class DuckAdapter implements Turkey{
+	
+	Duck duck;
+	
+	public DuckAdapter(Duck duck) {
+		this.duck = duck;
+	}
+
+	@Override
+	public void gobble() {
+		
+		duck.quack();
+		
+	}
+
+	@Override
+	public void fly() {
+		
+		
+	}
+}
+	
+	
